42e1d52bf1bcc3c558c31ea635626383d7f96d72
Cece Tran <cecetran@Ceces-MacBook-Air.local>
2016-02-10 15:34:21 -0800

    New scraper for pinellas,FL and made minor updates to hillsborough scraper to fix small bugs dealing with empty fields on website

--- PinellasFL/pinellas_scraper.py
+++ PinellasFL/pinellas_scraper.py
@@ -73,7 +73,7 @@ class Scraper:
 	def acquire_field_text(self, xpath):
 		return self.driver.find_element_by_xpath(xpath).text
 
-	def write_row(self, case_number, case_type, case_status, court, file_date, judicial_officer, petitioner, petitioner_attorney, respondent, respondent_attorney):
+	def write_row(self, case_number, case_type, case_status, court, file_date, judicial_officer, petitioner, petitioner_address, petitioner_attorney, respondent, respondent_address, respondent_attorney):
 		with open(self.file_name + '.csv', 'a') as csvfile:
 			self.writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
 			self.writer.writerow({'case_number' : case_number, 
@@ -83,34 +83,36 @@ class Scraper:
 							 	  'file_date' : file_date,
 							 	  'judicial_officer' : judicial_officer,
 							 	  'petitioner' : petitioner,
+							 	  'petitioner_address' : petitioner_address,
 							 	  'petitioner_attorney' : petitioner_attorney,
 							 	  'respondent' : respondent,
+							 	  'respondent_address' : respondent_address,
 							 	  'respondent_attorney': respondent_attorney})
 
 
 def main():
-	#Superior Court of Florida, County of Hillsborough
-	url = 'http://pubrec10.hillsclerk.com/Unsecured/default.aspx'
-	status = "Closed"
+	#Superior Court of Florida, County of Pinellas
+	url = 'https://ccmspa.pinellascounty.org/PublicAccess/default.aspx'
+	status = "Open"
 	date_start = date(2014, 1, 1)
 	date_end = date(2014, 12, 31)
-	interval = 14
-	county = "HillsboroughFL"
+	interval = 7
+	county = "PinellasFL"
 
 	#initialize Scraper class
-	hills = Scraper(url, status, date_start, date_end, county)
+	scraper = Scraper(url, status, date_start, date_end, county)
 
 	#create initial csv with fieldnames
-	fieldnames = ['case_number', 'case_type', 'case_status', 'court', 'file_date', 'judicial_officer', 'petitioner', 'petitioner_attorney', 'respondent', 'respondent_attorney']
-	hills.create_csv(fieldnames)
+	fieldnames = ['case_number', 'case_type', 'case_status', 'court', 'file_date', 'judicial_officer', 'petitioner','petitioner_address', 'petitioner_attorney', 'respondent','respondent_address', 'respondent_attorney']
+	scraper.create_csv(fieldnames)
 
 	#initialize browser window (either firefox or phantomjs)
 	wait_time = 30
-	#hills.init_firefox(wait_time)
-	hills.init_phantom(wait_time)
+	#scraper.init_firefox(wait_time)
+	scraper.init_phantom(wait_time)
 
 	#navigate to search page
-	hills.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/a[2]').click()
+	scraper.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/a[3]').click()
 
 	current_date = date_start
 
@@ -121,93 +123,109 @@ def main():
 		else:
 			next_date = current_date + timedelta(days=interval)
 
-		hills.set_date_interval(current_date, next_date)
+		scraper.set_date_interval(current_date, next_date)
 
 
 		#navigate to the search page and click on anything needed
 		if (status == "Open"):
-			xpaths = ['//*[@id="DateFiled"]', '//*[@id="OpenOption"]', '/html/body/form/table[4]/tbody/tr/td/table/tbody/tr[11]/td[2]/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/select/option[10]']
+			xpaths = ['//*[@id="DateFiled"]', '//*[@id="OpenOption"]', '/html/body/form/table[4]/tbody/tr/td/table/tbody/tr[11]/td[2]/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/select/option[7]']
 		elif (status == "Closed"):
-			xpaths = ['//*[@id="DateFiled"]', '//*[@id="ClosedOption"]', '/html/body/form/table[4]/tbody/tr/td/table/tbody/tr[11]/td[2]/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/select/option[10]']
-		hills.navigate(xpaths)
+			xpaths = ['//*[@id="DateFiled"]', '//*[@id="ClosedOption"]', '/html/body/form/table[4]/tbody/tr/td/table/tbody/tr[11]/td[2]/table/tbody/tr/td/div/table/tbody/tr/td/table/tbody/tr/td[2]/select/option[7]']
+		scraper.navigate(xpaths)
 
 		#fill out form and search
 		xpaths = ['//*[@id="DateFiledOnAfter"]', '//*[@id="DateFiledOnBefore"]', '//*[@id="SearchSubmit"]']
-		hills.search(xpaths)
+		scraper.search(xpaths)
 
-		print "Date Range: " + hills.formatted_date(hills.date_start, 'slash') + ' - ' + hills.formatted_date(hills.date_end, 'slash')
+		print "Date Range: " + scraper.formatted_date(scraper.date_start, 'slash') + ' - ' + scraper.formatted_date(scraper.date_end, 'slash')
 
 		#get total number of records
-		record_count = int(hills.acquire_field_text('/html/body/table[3]/tbody/tr[1]/td[2]/b'))
+		record_count = int(scraper.acquire_field_text('/html/body/table[3]/tbody/tr[1]/td[2]/b'))
 		print "Total records: " + str(record_count)
 
 		count = 0
 
 		while count < record_count:
 
-			xpath = '/html/body/table[4]/tbody/tr[' + str(count + 2) + ']/td[1]/a'
-			case_num = hills.acquire_field_text(xpath)
-			hills.driver.find_element_by_xpath(xpath).click()
+			xpath = '/html/body/table[4]/tbody/tr[' + str(count + 3) + ']/td[1]/a'
+			case_num = scraper.acquire_field_text(xpath)
+			scraper.driver.find_element_by_xpath(xpath).click()
 
 			field_xpath = '/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[1]/td/b'
-			case_type = hills.acquire_field_text(field_xpath)
+			case_type = scraper.acquire_field_text(field_xpath)
 
 			field_xpath = '/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[2]/td/b'
-			date_filed = hills.acquire_field_text(field_xpath)
+			date_filed = scraper.acquire_field_text(field_xpath)
 
 			field_xpath = '/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[3]/td/b'
-			location = hills.acquire_field_text(field_xpath)
+			location = scraper.acquire_field_text(field_xpath)
 
-			try:
-				field_xpath = '/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[4]/td/b'
-				judicial_officer = hills.acquire_field_text(field_xpath)
-			except:
-				judicial_officer = ""
-				print "hmm"
+			field_xpath = '/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[4]/td/b'
+			judicial_officer = scraper.acquire_field_text(field_xpath)
 
-			hills.set_timer(0.3)
+			scraper.set_timer(0.3)
 			try:
 				try:
 					field_xpath = '//*[@id="PIr11"]'
-					petitioner = hills.acquire_field_text(field_xpath)
+					petitioner = scraper.acquire_field_text(field_xpath)
 				except:
 					field_xpath = '//*[@id="PIr12"]'
-					petitioner = hills.acquire_field_text(field_xpath)
+					petitioner = scraper.acquire_field_text(field_xpath)
+
+				try:
+					field_xpath = '/html/body/table[4]/tbody/tr[3]/td'
+					p_address = cleanse(scraper.acquire_field_text(field_xpath))
+				except:
+					try:
+						field_xpath = '/html/body/table[5]/tbody/tr[3]/td'
+						p_address = cleanse(scraper.acquire_field_text(field_xpath))
+					except:
+						print "CHECK RESPONDENT ADDRESS FOR " + case_num
 
 				try:
 					field_xpath = '//*[@id="PIr12"]'
-					respondent = hills.acquire_field_text(field_xpath)
+					respondent = scraper.acquire_field_text(field_xpath)
 				except:
 					field_xpath = '//*[@id="PIr13"]'
-					respondent = hills.acquire_field_text(field_xpath)
+					respondent = scraper.acquire_field_text(field_xpath)
+
+				try:
+					field_xpath = '/html/body/table[4]/tbody/tr[6]/td'
+					r_address = cleanse(scraper.acquire_field_text(field_xpath))
+				except:
+					try:
+						field_xpath = '/html/body/table[5]/tbody/tr[6]/td'
+						r_address = cleanse(scraper.acquire_field_text(field_xpath))
+					except:
+						print "CHECK RESPONDENT ADDRESS FOR " + case_num
 
 				try:
 					field_xpath = '/html/body/table[4]/tbody/tr[2]/td[3]'
-					petitioner_attorney = cleanse(hills.acquire_field_text(field_xpath))
+					petitioner_attorney = cleanse(scraper.acquire_field_text(field_xpath))
 
 					field_xpath = '/html/body/table[4]/tbody/tr[5]/td[3]'
-					respondent_attorney = cleanse(hills.acquire_field_text(field_xpath))
+					respondent_attorney = cleanse(scraper.acquire_field_text(field_xpath))
 				except:
 					field_xpath = '/html/body/table[5]/tbody/tr[2]/td[3]'
-					petitioner_attorney = cleanse(hills.acquire_field_text(field_xpath))
+					petitioner_attorney = cleanse(scraper.acquire_field_text(field_xpath))
 
 					field_xpath = '/html/body/table[5]/tbody/tr[5]/td[3]'
-					respondent_attorney = cleanse(hills.acquire_field_text(field_xpath))
+					respondent_attorney = cleanse(scraper.acquire_field_text(field_xpath))
 			except:
 				print "Entry " + case_num + " missing critical information. Discarded."
 
-			hills.set_timer(30)
+			scraper.set_timer(30)
 
 			print "Writing " + case_num
-			hills.write_row(case_num, case_type, status, location, date_filed, judicial_officer, petitioner, petitioner_attorney, respondent, respondent_attorney)
+			scraper.write_row(case_num, case_type, status, location, date_filed, judicial_officer, petitioner, p_address, petitioner_attorney, respondent, r_address, respondent_attorney)
 
 			count += 1
-			hills.driver.back()
+			scraper.driver.back()
 
 		print "Saved " + str(count) + " records"
 
 		#go back to the search form
-		hills.driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[1]/font/a[5]').click()
+		scraper.driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[1]/font/a[5]').click()
 
 		current_date = next_date + timedelta(days=1)
 
